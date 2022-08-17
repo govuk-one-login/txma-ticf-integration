b7f861b266c7cf39cc53f08ca93f1d94b514846f
Andy Langridge <andy.langridge@digital.cabinet-office.gov.uk>
2022-08-17 11:00:15 +0100

    Remove unnecessary options from jest config - ts-jest preset covers these

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,7 @@ import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
-  moduleFileExtensions: ['js', 'ts'],
   preset: 'ts-jest',
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
   verbose: true
 }
 
