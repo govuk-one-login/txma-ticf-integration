85c8f5004e690d215adbee2355bed26115228f43
Andy Langridge <andy.langridge@digital.cabinet-office.gov.uk>
2022-08-17 10:54:13 +0100

    Add coverage folder to prettier and eslint ignore files

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,10 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
   moduleFileExtensions: ['js', 'ts'],
+  preset: 'ts-jest',
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
-  preset: 'ts-jest',
   verbose: true
 }
 
