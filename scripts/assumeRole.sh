f977314b5d43196fca3beff467c0fd1e9781d964
Andy Langridge <andy.langridge@digital.cabinet-office.gov.uk>
2022-08-18 12:19:33 +0100

    Add an assume role script

---
+++ scripts/assumeRole.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# To run this script requires you to have the following environment variables set:
+# GDS_EMAIL_ADDRESS - the email address of the account for the 'gds-users' AWS account
+# ROLE_ARN - the ARN of the role you want to assume in the relevant AWS account
+: "${GDS_EMAIL_ADDRESS:?GDS_EMAIL_ADDRESS not set or empty}"
+: "${ROLE_ARN:?ROLE_ARN not set or empty}"
+
+# Pass in the MFA code as the first argument when running the script
+# e.g.
+# source ./scripts/assumeRole.sh 123456
+MFA_CODE=$1
+
+ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+MFA_DEVICE_ARN=arn:aws:iam::${ACCOUNT_ID}:mfa/$GDS_EMAIL_ADDRESS
+MFA_CREDENTIALS=$(aws sts get-session-token --serial-number $MFA_DEVICE_ARN --token-code $1)
+ASSUMED_ROLE=$(AWS_ACCESS_KEY_ID=$(echo $MFA_CREDENTIALS | jq -r '.Credentials.AccessKeyId') AWS_SECRET_ACCESS_KEY=$(echo $MFA_CREDENTIALS | jq -r '.Credentials.SecretAccessKey') AWS_SESSION_TOKEN=$(echo $MFA_CREDENTIALS | jq -r '.Credentials.SessionToken') aws sts assume-role --role-arn $ROLE_ARN --role-session-name session)
+
+export AWS_ACCESS_KEY_ID=$(echo $ASSUMED_ROLE | jq -r '.Credentials.AccessKeyId')
+export AWS_SECRET_ACCESS_KEY=$(echo $ASSUMED_ROLE | jq -r '.Credentials.SecretAccessKey')
+export AWS_SESSION_TOKEN=$(echo $ASSUMED_ROLE | jq -r '.Credentials.SessionToken')
+
+echo "AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID"
+echo "AWS_SECRET_ACCESS_KEY_ID: $AWS_SECRET_ACCESS_KEY"
+echo "AWS_SESSION_TOKEN: $AWS_SESSION_TOKEN"
