3f074c7254d205ae72964b05c1bbf2ee36fe91ad
andrewmccafferty <andrew.mccafferty@digital.cabinet-office.gov.uk>
2022-09-14 11:07:44 +0100

    Add return to SQS handler

--- src/handlers/processDataRequest.ts
+++ src/handlers/processDataRequest.ts
@@ -2,4 +2,5 @@ import { SQSEvent } from 'aws-lambda'
 
 export const handler = async (event: SQSEvent) => {
   console.log('Handling data request SQS event', JSON.stringify(event, null, 2))
+  return {}
 }
