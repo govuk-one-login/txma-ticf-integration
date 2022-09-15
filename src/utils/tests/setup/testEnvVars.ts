0f5000daef216ffe92606892df05ecfe643397f0
mabon-gds <110031583+mabon-gds@users.noreply.github.com>
2022-09-15 17:07:43 +0100

    Feature/secret access for branches (#59)

--- src/utils/tests/setup/testEnvVars.ts
+++ src/utils/tests/setup/testEnvVars.ts
@@ -1,3 +1,6 @@
+import { TEST_ZENDESK_SECRET_NAME } from '../testConstants'
+
 process.env.ANALYSIS_BUCKET_NAME = 'analysis-bucket'
 process.env.AUDIT_BUCKET_NAME = 'audit-bucket'
 process.env.AWS_REGION = 'eu-west-2'
+process.env.ZENDESK_API_SECRETS_NAME = TEST_ZENDESK_SECRET_NAME
