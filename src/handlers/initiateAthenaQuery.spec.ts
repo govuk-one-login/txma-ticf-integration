02215e9b84603cc814e5e87cb102bed9da111204
Tom Pickering <tom.pickering@digital.cabinet-office.gov.uk>
2022-09-15 13:52:42 +0100

    TT2-26 error handling in handler

--- src/handlers/initiateAthenaQuery.spec.ts
+++ src/handlers/initiateAthenaQuery.spec.ts
@@ -1,12 +1,30 @@
-// import { handler } from './initiateAthenaQuery'
-// import { confirmAthenaTable } from '../services/athena/confirmAthenaTable'
+import { handler } from './initiateAthenaQuery'
+import { confirmAthenaTable } from '../services/athena/confirmAthenaTable'
+import { ConfirmAthenaTableResult } from '../types/confirmAthenaTableResult'
+import { testAthenaQueryEvent } from '../utils/tests/events/initiateAthenaQueryEvent'
 
 jest.mock('../services/athena/confirmAthenaTable', () => ({
   confirmAthenaTable: jest.fn()
 }))
 
+const confirmAthenaTableMock = confirmAthenaTable as jest.Mock<
+  Promise<ConfirmAthenaTableResult>
+>
+
 describe('initiate athena query handler', () => {
-  it('has a placeholder', async () => {
-    expect(true).toBe(true)
+  beforeEach(() => {
+    confirmAthenaTableMock.mockReset()
+  })
+
+  it('confirms whether the athena data source exists', async () => {
+    await handler(testAthenaQueryEvent)
+    expect(confirmAthenaTableMock).toHaveBeenCalled()
+  })
+
+  it('throws an error if there is no data in the SQS Event', async () => {
+    expect(handler({ Records: [] })).rejects.toThrow(
+      'No data in Athena Query event'
+    )
+    expect(confirmAthenaTableMock).not.toHaveBeenCalled()
   })
 })
--- src/handlers/initiateAthenaQuery.ts
+++ src/handlers/initiateAthenaQuery.ts
@@ -8,6 +8,10 @@ const table: string = getEnv('ATHENA_TABLE_NAME')
 export const handler = async (event: SQSEvent): Promise<void> => {
   console.log('Handling Athena Query event', JSON.stringify(event, null, 2))
 
+  if (event.Records.length < 1) {
+    throw new Error('No data in Athena Query event')
+  }
+
   const input = {
     DatabaseName: database,
     Name: table
