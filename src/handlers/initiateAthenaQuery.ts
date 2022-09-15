ca4208cdbb65fdec10d05fa1a1cbb147b117665e
Tom Pickering <tom.pickering@digital.cabinet-office.gov.uk>
2022-09-15 10:32:12 +0100

    TT2-26: use of env vars in handler

--- src/handlers/initiateAthenaQuery.ts
+++ src/handlers/initiateAthenaQuery.ts
@@ -2,12 +2,15 @@ import { SQSEvent } from 'aws-lambda'
 import { confirmAthenaTable } from '../services/athena/confirmAthenaTable'
 import { getEnv } from '../utils/helpers'
 
+const database: string = getEnv('ATHENA_DATABASE_NAME')
+const table: string = getEnv('ATHENA_TABLE_NAME')
+
 export const handler = async (event: SQSEvent): Promise<void> => {
   console.log('Handling Athena Query event', JSON.stringify(event, null, 2))
 
   const input = {
-    DatabaseName: getEnv('ATHENA_DATABASE_NAME'),
-    Name: getEnv('ATHENA_TABLE_NAME')
+    DatabaseName: database,
+    Name: table
   }
 
   const athenaTableExists = await confirmAthenaTable(input)
