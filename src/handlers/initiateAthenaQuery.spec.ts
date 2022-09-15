7b490ad4dcf59d676e940a89f79b0327cf4d4d6b
Tom Pickering <tom.pickering@digital.cabinet-office.gov.uk>
2022-09-15 17:12:22 +0100

    TT2-26: refactor confirmAthenaTable service and tests

--- src/handlers/initiateAthenaQuery.spec.ts
+++ src/handlers/initiateAthenaQuery.spec.ts
@@ -2,34 +2,52 @@ import { handler } from './initiateAthenaQuery'
 import { confirmAthenaTable } from '../services/athena/confirmAthenaTable'
 import { ConfirmAthenaTableResult } from '../types/confirmAthenaTableResult'
 import { testAthenaQueryEvent } from '../utils/tests/events/initiateAthenaQueryEvent'
+// import { updateZendeskTicket } from '../services/updateZendeskTicket'
+
+// NOTE - updateZendeskTicket tests to be uncommented once trigger event finalised, see handler file
 
 jest.mock('../services/athena/confirmAthenaTable', () => ({
   confirmAthenaTable: jest.fn()
 }))
+// jest.mock('./services/updateZendeskTicket', () => ({
+//   updateZendeskTicket: jest.fn()
+// }))
 
-const confirmAthenaTableMock = confirmAthenaTable as jest.Mock<
+const mockConfirmAthenaTable = confirmAthenaTable as jest.Mock<
   Promise<ConfirmAthenaTableResult>
 >
+// const mockUpdateZendeskTicket = updateZendeskTicket as jest.Mock
 
 describe('initiate athena query handler', () => {
   beforeEach(() => {
-    confirmAthenaTableMock.mockReset()
+    mockConfirmAthenaTable.mockReset()
   })
 
   it('confirms whether the athena data source exists', async () => {
-    confirmAthenaTableMock.mockResolvedValue({
+    mockConfirmAthenaTable.mockResolvedValue({
       tableAvailable: true,
       message: 'test message'
     })
 
     await handler(testAthenaQueryEvent)
-    expect(confirmAthenaTableMock).toHaveBeenCalled()
+    expect(mockConfirmAthenaTable).toHaveBeenCalled()
   })
+
+  // it('closes the Zendesk ticket if the data source does not exist', async () => {
+  //   mockConfirmAthenaTable.mockResolvedValue({
+  //     tableAvailable: false,
+  //     message: 'test message'
+  //   })
+
+  //   await handler(testAthenaQueryEvent)
+  //   expect(mockConfirmAthenaTable).toHaveBeenCalled()
+  //   expect(mockUpdateZendeskTicket).toHaveBeenCalled()
+  // })
 
   it('throws an error if there is no data in the SQS Event', async () => {
     expect(handler({ Records: [] })).rejects.toThrow(
       'No data in Athena Query event'
     )
-    expect(confirmAthenaTableMock).not.toHaveBeenCalled()
+    expect(mockConfirmAthenaTable).not.toHaveBeenCalled()
   })
 })
--- src/handlers/initiateAthenaQuery.ts
+++ src/handlers/initiateAthenaQuery.ts
@@ -1,5 +1,6 @@
 import { SQSEvent } from 'aws-lambda'
 import { confirmAthenaTable } from '../services/athena/confirmAthenaTable'
+// import { updateZendeskTicket } from '../services/updateZendeskTicket'
 
 export const handler = async (event: SQSEvent): Promise<void> => {
   console.log('Handling Athena Query event', JSON.stringify(event, null, 2))
@@ -7,9 +8,16 @@ export const handler = async (event: SQSEvent): Promise<void> => {
   if (event.Records.length < 1) {
     throw new Error('No data in Athena Query event')
   }
-  const athenaTableExists = await confirmAthenaTable()
 
-  console.log(athenaTableExists.message)
+  const doesAthenaTableExist = await confirmAthenaTable()
+
+  // NOTE - This will need to be updated once the mechanism for retrieving the Zendesk Ticket information is finalised
+  // Zendesk Ticket ID could be passed in the SQS Event or could be retrieved from a database
+  // if (!doesAthenaTableExist.tableAvailable) {
+  //   await updateZendeskTicket('zendeskevent', doesAthenaTableExist.message, 'closed')
+  // }
+
+  console.log(doesAthenaTableExist.message)
 
   return
 }
