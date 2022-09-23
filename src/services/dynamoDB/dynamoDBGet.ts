20fd1455277d4cbc1b933dd428b3e0719e57769b
Sam Berry <sam.berry@digital.cabinet-office.gov.uk>
2022-09-23 17:01:40 +0100

    pass dynamodb table name into function

--- src/services/dynamoDB/dynamoDBGet.ts
+++ src/services/dynamoDB/dynamoDBGet.ts
@@ -1,9 +1,10 @@
 import { GetItemCommand } from '@aws-sdk/client-dynamodb'
+import { getEnv } from '../../utils/helpers'
 import { ddbClient } from './dynamoDBClient'
 
 export const getQueryByZendeskId = async (zendeskId: string) => {
   const params = {
-    TableName: 'your-table-name',
+    TableName: getEnv('DYNAMODB_TABLE_NAME'),
     Key: { zendeskId: { S: zendeskId } }
   }
 
