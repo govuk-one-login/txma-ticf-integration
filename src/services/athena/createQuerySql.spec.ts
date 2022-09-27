a470defd4052b5e24002a836d7f059f4e6815658
Tom Pickering <tom.pickering@digital.cabinet-office.gov.uk>
2022-09-27 19:10:24 +0100

    TT2-17 sort by identifier type

--- src/services/athena/createQuerySql.spec.ts
+++ src/services/athena/createQuerySql.spec.ts
@@ -1,7 +1,10 @@
 import { createQuerySql } from './createQuerySql'
+import { testDataRequest } from '../../utils/tests/testDataRequest'
 
 describe('create Query SQL', () => {
   test('returns a string', () => {
-    expect(createQuerySql({ Item: {}, $metadata: {} })).toEqual('SQL string')
+    expect(createQuerySql(testDataRequest)).toEqual(
+      'SELECT * FROM test_database.test_table'
+    )
   })
 })
--- src/services/athena/createQuerySql.ts
+++ src/services/athena/createQuerySql.ts
@@ -1,7 +1,31 @@
-import { GetItemCommandOutput } from '@aws-sdk/client-dynamodb'
+import { DataRequestParams } from '../../types/dataRequestParams'
+import { getEnv } from '../../utils/helpers'
 
-export const createQuerySql = (requestData: GetItemCommandOutput): string => {
-  console.log(requestData)
+export const createQuerySql = (requestData: DataRequestParams): string => {
+  const identifiers = getIdentifiers(requestData)
 
-  return 'SQL string'
+  console.log(identifiers)
+
+  const dataSource = `${getEnv('ATHENA_DATABASE_NAME')}.${getEnv(
+    'ATHENA_TABLE_NAME'
+  )}`
+
+  const queryString = `SELECT * FROM ${dataSource}`
+
+  return queryString
+}
+
+const getIdentifiers = (requestData: DataRequestParams): string[] => {
+  const identifierType = requestData.identifierType
+
+  if (identifierType === 'event_id' && requestData.eventIds) {
+    return requestData.eventIds
+  } else if (identifierType === 'journey_id' && requestData.journeyIds) {
+    return requestData.journeyIds
+  } else if (identifierType === 'session_id' && requestData.sessionIds) {
+    return requestData.sessionIds
+  } else if (identifierType === 'user_id' && requestData.userIds) {
+    return requestData.userIds
+  }
+  return []
 }
