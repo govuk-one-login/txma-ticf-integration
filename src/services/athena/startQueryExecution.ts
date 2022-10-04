9dc35ab09ea4bcf972ebb52d4cb712a72c5fbb80
Tom Pickering <tom.pickering@digital.cabinet-office.gov.uk>
2022-10-04 13:05:57 +0100

    TT2-18 amend return type, handle failed execution

--- src/services/athena/startQueryExecution.ts
+++ src/services/athena/startQueryExecution.ts
@@ -3,29 +3,35 @@ import {
   StartQueryExecutionCommand,
   StartQueryExecutionCommandInput
 } from '@aws-sdk/client-athena'
-import { CreateQuerySqlResult } from '../../types/createQuerySqlResult'
+import { CreateQuerySqlResult } from '../../types/athena/createQuerySqlResult'
+import { StartQueryExecutionResult } from '../../types/athena/startQueryExecutionResult'
 import { getEnv } from '../../utils/helpers'
 
 export const startQueryExecution = async (
   queryParams: CreateQuerySqlResult
-): Promise<string> => {
-  // create athena Client
-
+): Promise<StartQueryExecutionResult> => {
   // validate queryParams
+  console.log('Attempting to start Athena query execution')
 
   const client = new AthenaClient({ region: getEnv('AWS_REGION') })
 
-  // make query
   const input = generateQueryExecutionCommandInput(queryParams)
-
-  // execute query
 
   const command = new StartQueryExecutionCommand(input)
 
-  //
   const response = await client.send(command)
 
-  return response.QueryExecutionId
+  if (!response.QueryExecutionId) {
+    return {
+      queryExecuted: false,
+      error: 'Athena query execution initiation failed'
+    }
+  }
+
+  return {
+    queryExecuted: true,
+    queryExecutionId: response.QueryExecutionId
+  }
 }
 
 const generateQueryExecutionCommandInput = (
