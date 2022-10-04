b1b34510412adfef7d7e2940aaf5635a37abf52d
Tom Pickering <tom.pickering@digital.cabinet-office.gov.uk>
2022-10-04 17:32:00 +0100

    TT2-18 unit tests

--- src/services/athena/startQueryExecution.spec.ts
+++ src/services/athena/startQueryExecution.spec.ts
@@ -1,29 +1,44 @@
-// import { startQueryExecution } from './startQueryExecution'
-// import { mockClient } from 'aws-sdk-client-mock'
-// import { AthenaClient, StartQueryExecutionCommand } from '@aws-sdk/client-athena'
+import { startQueryExecution } from './startQueryExecution'
+import { mockClient } from 'aws-sdk-client-mock'
+import {
+  AthenaClient,
+  StartQueryExecutionCommand
+} from '@aws-sdk/client-athena'
 
-// const athenaMock = mockClient(AthenaClient)
+const athenaMock = mockClient(AthenaClient)
 
-// describe('start Query execution', () => {
-//   test('returns a QueryExecutionId if a query is succefully initiated', () => {
-//     expect(startQueryExecution({sqlGenerated: true, idParameters: ['test_parameter'], sql: 'test sql'})).toEqual({
-//       sqlGenerated: true,
-//       sql: "SELECT json_extract(restricted, '$.user.firstName') as user_firstname, json_extract(restricted, '$.user.lastName') as user_lastname FROM test_database.test_table WHERE event_id=? OR event_id=?",
-//       idParameters: ['123', '456']
-//     })
-//   })
+describe('start Query execution', () => {
+  beforeEach(() => {
+    athenaMock.reset()
+  })
 
-//   // test('returns an error message if there are no dataPaths', () => {
-//   //   expect(createQuerySql(testDataRequest)).toEqual({
-//   //     sqlGenerated: false,
-//   //     error: 'No dataPaths in request'
-//   //   })
-//   // })
+  test('returns a QueryExecutionId if a query is successfully initiated', async () => {
+    athenaMock.on(StartQueryExecutionCommand).resolves({
+      QueryExecutionId: '123'
+    })
 
-//   // test('returns an error message if there are no ids corresponding to the identifier type', () => {
-//   //   expect(createQuerySql(noEventIdTestDataRequest)).toEqual({
-//   //     sqlGenerated: false,
-//   //     error: 'No ids of type: event_id'
-//   //   })
-//   // })
-// })
+    const result = await startQueryExecution({
+      sqlGenerated: true,
+      idParameters: ['test_parameter'],
+      sql: 'test sql'
+    })
+    expect(result).toEqual({
+      queryExecuted: true,
+      queryExecutionId: '123'
+    })
+  })
+
+  test('returns an error message if a query is not initiated', async () => {
+    athenaMock.on(StartQueryExecutionCommand).resolves({})
+
+    const result = await startQueryExecution({
+      sqlGenerated: true,
+      idParameters: ['test_parameter'],
+      sql: 'test sql'
+    })
+    expect(result).toEqual({
+      queryExecuted: false,
+      error: 'Athena query execution initiation failed'
+    })
+  })
+})
