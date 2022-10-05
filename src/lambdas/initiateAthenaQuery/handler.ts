f587ac6a89c6ea3e9c97a89f4e0b4f93d3dd8b3a
SamBerry-GDS <110039739+SamBerry-GDS@users.noreply.github.com>
2022-10-05 16:27:08 +0100

    Groups handler and service functions (#83)
    * Groups handler and service functions
    
    * Changes entry point file names to handler
    
    * fix handler path in webpack

---
+++ src/lambdas/initiateAthenaQuery/handler.ts
@@ -0,0 +1,33 @@
+import { SQSEvent } from 'aws-lambda'
+import { confirmAthenaTable } from '../../sharedServices/athena/confirmAthenaTable'
+import { getQueryByZendeskId } from '../../sharedServices/dynamoDB/dynamoDBGet'
+import { updateZendeskTicketById } from '../../sharedServices/zendesk/updateZendeskTicket'
+
+export const handler = async (event: SQSEvent): Promise<void> => {
+  console.log('Handling Athena Query event', JSON.stringify(event, null, 2))
+
+  if (event.Records.length < 1) {
+    throw new Error('No data in Athena Query event')
+  }
+
+  const zendeskId = event.Records[0].body
+  if (zendeskId.length < 1) {
+    throw new Error('No zendeskId recieved from SQS')
+  }
+
+  const doesAthenaTableExist = await confirmAthenaTable()
+
+  if (!doesAthenaTableExist.tableAvailable) {
+    await updateZendeskTicketById(
+      zendeskId,
+      doesAthenaTableExist.message,
+      'closed'
+    )
+    throw new Error(doesAthenaTableExist.message)
+  }
+
+  const requestData = await getQueryByZendeskId(zendeskId)
+  console.log(requestData)
+
+  return
+}
