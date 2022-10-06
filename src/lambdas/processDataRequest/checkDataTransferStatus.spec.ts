cf38d97be8d0b649544b55c0c5e50776689205f5
andrewmccafferty <andrew.mccafferty@digital.cabinet-office.gov.uk>
2022-10-06 20:47:11 +0100

    Add skeleton tests for checkDataTransfer

---
+++ src/lambdas/processDataRequest/checkDataTransferStatus.spec.ts
@@ -0,0 +1,94 @@
+import // getDbEntryByZendeskId,
+// getQueryByZendeskId
+'../../sharedServices/dynamoDB/dynamoDBGet'
+import { sendContinuePollingDataTransferMessage } from '../../sharedServices/queue/sendContinuePollingDataTransferMessage'
+import { checkS3BucketData } from '../../sharedServices/s3/checkS3BucketData'
+import { startCopyJob } from '../../sharedServices/bulkJobs/startCopyJob'
+import { sendInitiateAthenaQueryMessage } from '../../sharedServices/queue/sendInitiateAthenaQueryMessage'
+import { checkDataTransferStatus } from './checkDataTransferStatus'
+import { when } from 'jest-when'
+import { ZENDESK_TICKET_ID } from '../../utils/tests/testConstants'
+
+jest.mock('../../sharedServices/dynamoDB/dynamoDBGet', () => ({
+  getQueryByZendeskId: jest.fn()
+}))
+
+jest.mock(
+  '../../sharedServices/queue/sendContinuePollingDataTransferMessage',
+  () => ({
+    sendContinuePollingDataTransferMessage: jest.fn()
+  })
+)
+
+jest.mock('../../sharedServices/s3/checkS3BucketData', () => ({
+  checkS3BucketData: jest.fn()
+}))
+
+describe('checkDataTransferStatus', () => {
+  beforeAll(() => {
+    jest.resetAllMocks()
+  })
+
+  const givenDataResult = (
+    standardTierLocationsToCopy: string[],
+    glacierTierLocationsToCopy: string[]
+  ) => {
+    when(checkS3BucketData).mockResolvedValue({
+      standardTierLocationsToCopy: standardTierLocationsToCopy,
+      glacierTierLocationsToCopy: glacierTierLocationsToCopy,
+      dataAvailable: true
+    })
+  }
+  const filesToCopy = ['file-to-copy1', 'file-to-copy-2']
+  const givenGlacierDefrostPending = () => {
+    givenDataResult(filesToCopy, [
+      'glacier-to-restore-1',
+      'glacier-to-restore-2'
+    ])
+  }
+
+  const givenCopyRequired = () => {
+    givenDataResult(filesToCopy, [])
+  }
+
+  const givenDataReadyForQuery = () => {
+    givenDataResult([], [])
+  }
+
+  // TODO: all these tests need to access the database
+
+  xit('should continue polling if a glacier defrost is pending', async () => {
+    givenGlacierDefrostPending()
+    await checkDataTransferStatus(ZENDESK_TICKET_ID)
+    expect(sendContinuePollingDataTransferMessage).toBeCalledWith(
+      ZENDESK_TICKET_ID
+    )
+    // TODO: add expectation for checkGlacierStatusCount to be incremented
+  })
+
+  xit('should start copy from audit to analysis bucket if no glacier defrost is pending and there are files to copy', async () => {
+    givenCopyRequired()
+    await checkDataTransferStatus(ZENDESK_TICKET_ID)
+    expect(startCopyJob).toBeCalledWith(filesToCopy, ZENDESK_TICKET_ID)
+    expect(sendContinuePollingDataTransferMessage).toBeCalledWith(
+      ZENDESK_TICKET_ID
+    )
+  })
+
+  xit('should continue to wait if there are pending files to copy from audit bucket and a copy has already started', async () => {
+    // TODO: this will depend on us looking at the database record and checking for the presence of the checkCopyStatusCount flag,
+    // which will tell us a copy has already started
+    givenCopyRequired()
+    await checkDataTransferStatus(ZENDESK_TICKET_ID)
+    expect(sendContinuePollingDataTransferMessage).toBeCalledWith(
+      ZENDESK_TICKET_ID
+    )
+    // TODO: add expectation for checkCopyStatusCount to be incremented
+  })
+
+  xit('should queue athena query if all data is now ready', async () => {
+    givenDataReadyForQuery()
+    await checkDataTransferStatus(ZENDESK_TICKET_ID)
+    expect(sendInitiateAthenaQueryMessage).toBeCalledWith(ZENDESK_TICKET_ID)
+  })
+})
--- src/lambdas/processDataRequest/checkDataTransferStatus.ts
+++ src/lambdas/processDataRequest/checkDataTransferStatus.ts
@@ -3,6 +3,7 @@ import {
   getQueryByZendeskId
 } from '../../sharedServices/dynamoDB/dynamoDBGet'
 import { sendContinuePollingDataTransferMessage } from '../../sharedServices/queue/sendContinuePollingDataTransferMessage'
+import { sendInitiateAthenaQueryMessage } from '../../sharedServices/queue/sendInitiateAthenaQueryMessage'
 import { checkS3BucketData } from '../../sharedServices/s3/checkS3BucketData'
 
 export const checkDataTransferStatus = async (zendeskId: string) => {
@@ -14,13 +15,15 @@ export const checkDataTransferStatus = async (zendeskId: string) => {
 
   const glacierRestoreStillInProgress =
     s3BucketDataLocationResult.glacierTierLocationsToCopy.length > 0
+
+  // TODO: start copy if glacier restore finished and this is required
   const copyJobStillInProgress =
     s3BucketDataLocationResult.standardTierLocationsToCopy.length > 0
 
-  if (glacierRestoreStillInProgress) {
-    return sendContinuePollingDataTransferMessage(zendeskId)
-  }
-  if (copyJobStillInProgress) {
-    return sendContinuePollingDataTransferMessage(zendeskId)
+  // TODO: increment retry counts
+  if (glacierRestoreStillInProgress || copyJobStillInProgress) {
+    await sendContinuePollingDataTransferMessage(zendeskId)
+  } else if (!glacierRestoreStillInProgress && !copyJobStillInProgress) {
+    await sendInitiateAthenaQueryMessage(zendeskId)
   }
 }
