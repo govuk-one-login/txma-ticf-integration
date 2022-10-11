d00ff41513256eac8b2d5e668fb37857fc02929d
mabon-gds <mabon.gwyn@digital.cabinet-office.gov.uk>
2022-10-11 16:32:14 +0100

    added a test for dynamoDBUpdate

--- src/sharedServices/dynamoDB/dynamoDBUpdate.spec.ts
+++ src/sharedServices/dynamoDB/dynamoDBUpdate.spec.ts
@@ -1,19 +1,26 @@
-// import { ZENDESK_TICKET_ID } from '../../utils/tests/testConstants'
-// import { incrementObjectFieldByOne } from './dynamoDBUpdate'
+import { incrementObjectFieldByOne } from './dynamoDBUpdate'
+import {
+  TEST_QUERY_DATABASE_TABLE_NAME,
+  ZENDESK_TICKET_ID
+} from '../../utils/tests/testConstants'
+import { DynamoDBClient, UpdateItemCommand } from '@aws-sdk/client-dynamodb'
+import { mockClient } from 'aws-sdk-client-mock'
 
-// const myFieldToUpdate = 'myFieldToUpdate'
-
-jest.mock('@aws-sdk/client-dynamodb', () => ({
-  DynamoDBClient: jest.fn().mockImplementation(() => {
-    return { send: mockSend }
-  })
-}))
-const mockSend = jest.fn()
+const dynamoMock = mockClient(DynamoDBClient)
+const myFieldToUpdate = 'myFieldToUpdate'
+const testParams = {
+  TableName: TEST_QUERY_DATABASE_TABLE_NAME,
+  Key: { zendeskId: { S: ZENDESK_TICKET_ID } },
+  UpdateExpression: `ADD ${myFieldToUpdate} :increment`,
+  ExpressionAttributeValues: {
+    ':increment': { N: '1' }
+  }
+}
 
 describe('incrementObjectFieldByOne', () => {
-  it('', async () => {
-    // await incrementObjectFieldByOne(ZENDESK_TICKET_ID, myFieldToUpdate)
-    // dynamoMock.commandCalls(UpdateItemCommand)
-    // expect(dynamoMock).toHaveBeenCalledWith(mockUpdateParams)
+  it('should call the send function with the correct parameters', async () => {
+    await incrementObjectFieldByOne(ZENDESK_TICKET_ID, myFieldToUpdate)
+
+    expect(dynamoMock).toHaveReceivedCommandWith(UpdateItemCommand, testParams)
   })
 })
