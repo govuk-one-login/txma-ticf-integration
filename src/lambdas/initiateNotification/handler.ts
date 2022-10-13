399b10e1261f943875d04e806e78d719b30c0e13
Tom Pickering <tom.pickering@digital.cabinet-office.gov.uk>
2022-10-13 17:40:45 +0100

    event detail type

--- src/lambdas/initiateNotification/handler.ts
+++ src/lambdas/initiateNotification/handler.ts
@@ -1,10 +1,20 @@
 import { EventBridgeEvent } from 'aws-lambda'
 
 export const handler = async (
-  event: EventBridgeEvent<'Athena Query State Change', 'detail'>
+  event: EventBridgeEvent<'Athena Query State Change', AthenaEventDetails>
 ): Promise<void> => {
   console.log(event)
 
-  const details = event.detail
-  console.log(details)
+  const athenaQueryId = event.detail.queryExecutionId
+  console.log(athenaQueryId)
+}
+
+interface AthenaEventDetails {
+  versionId: string
+  currentState: string
+  previousState: string
+  statementType: string
+  queryExecutionId: string
+  workgroupName: string
+  sequenceNumber: string
 }
