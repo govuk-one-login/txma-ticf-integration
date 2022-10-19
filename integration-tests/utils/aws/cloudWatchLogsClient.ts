1144fa7d628fdde96bc1c45701de5da040d7cdd2
Andy Langridge <andy.langridge@digital.cabinet-office.gov.uk>
2022-10-19 14:11:32 +0100

    Added integration tests that check Copy files flow (#102)
    * Add integration tests that check copy status
    
    * Fix assertions
    
    * Remove code used for local testing
    
    * Standardise assert functions
    
    * Remove debug code from S3 function
    
    * Rename invalid test case
    
    * Move clear data steps to before hook
    
    * Decrease test timeout value

--- integration-tests/utils/aws/cloudWatchLogsClient.ts
+++ integration-tests/utils/aws/cloudWatchLogsClient.ts
@@ -2,5 +2,5 @@ import { CloudWatchLogsClient } from '@aws-sdk/client-cloudwatch-logs'
 import { AWS_REGION } from '../../constants/awsParameters'
 
 export const cloudWatchLogsClient = new CloudWatchLogsClient({
-  region: `${AWS_REGION}`
+  region: AWS_REGION
 })
