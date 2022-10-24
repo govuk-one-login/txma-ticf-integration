48f046d3f0324894800faeaec63150d1257fc39c
Omoribake Ikumoluyi <omoribake.ikumoluyi@digital.cabinet-office.gov.uk>
2022-10-24 18:13:48 +0100

    Scenarios for data path, pii types and both. Helper objects for each scenario
    Signed-off-by: Omoribake Ikumoluyi <omoribake.ikumoluyi@digital.cabinet-office.gov.uk>

--- integration-tests/athenaQuery.spec.ts
+++ integration-tests/athenaQuery.spec.ts
@@ -12,6 +12,7 @@ import { createZendeskTicket } from './utils/zendesk/createZendeskTicket'
 import { validRequestData } from './constants/requestData'
 import {
   ANALYSIS_BUCKET_NAME,
+  ATHENA_QUERY_DATA_TEST_DATE_PREFIX,
   ATHENA_QUERY_TEST_FILE_NAME,
   INITIATE_ATHENA_QUERY_LAMBDA_LOG_GROUP,
   INITIATE_ATHENA_QUERY_QUEUE_URL
@@ -19,31 +20,42 @@ import {
 import { deleteZendeskTicket } from './utils/zendesk/deleteZendeskTicket'
 import { generateRandomNumber } from './utils/helpers'
 import { copyAuditDataFromTestDataBucket } from './utils/aws/s3CopyAuditDataFromTestDataBucket'
+import {
+  dynamoDBItemDataPathAndPIITypes,
+  dynamoDBItemDataPathsOnly,
+  dynamoDBItemPIITypesOnly
+} from './constants/dynamoDBItemDetails'
+import { deleteAuditDataWithPrefix } from './utils/aws/s3DeleteAuditDataWithPrefix'
 
-// TODO: ADD TO SET UP: - COPY DATA FROM TEST BUCKET TO ANALYSIS BUCKET, ENSURING DATE AND CONTENT MATCH ZENDESK TICKET DATE
-// TODO: add test for request without data paths when TT2-76 has been implemented
-
+// TODO: ADD TO SET UP: - COPY DATA FROM TEST BUCKET TO ANALYSIS BUCKET, ENSURING DATE IN TEST FILE AND CONTENT MATCH DYNAMODB TICKET DATE
 describe('Athena Query SQL generation and execution', () => {
   jest.setTimeout(90000)
 
   describe('Query SQL generation and execution successful', () => {
     const randomTicketId = (Number(generateRandomNumber()) * 1000).toString()
-    beforeAll(async () => {
-      await populateDynamoDBWithTestItemDetails(randomTicketId)
-      //TODO: CONTINUE MONDAY
-      copyAuditDataFromTestDataBucket(
+
+    beforeEach(async () => {
+      await deleteAuditDataWithPrefix(
         ANALYSIS_BUCKET_NAME,
-        'PATH_TO_NEW_FILE_THAT_I_WILL_PUT_INTO_THE_TEST_BUCKET',
+        `firehose/${ATHENA_QUERY_DATA_TEST_DATE_PREFIX}`
+      )
+      await copyAuditDataFromTestDataBucket(
+        ANALYSIS_BUCKET_NAME,
+        `firehose/${ATHENA_QUERY_DATA_TEST_DATE_PREFIX}/01/${ATHENA_QUERY_TEST_FILE_NAME}`,
         ATHENA_QUERY_TEST_FILE_NAME
       )
     })
 
-    afterAll(async () => {
+    afterEach(async () => {
       await deleteDynamoDBTestItem(randomTicketId)
     })
 
-    it('Event successfully received in Audit Queue should trigger Athena SQL lambda', async () => {
+    it('Successful Athena processing - requests having only data paths', async () => {
       console.log('Test ticket id: ' + randomTicketId)
+      await populateDynamoDBWithTestItemDetails(
+        randomTicketId,
+        dynamoDBItemDataPathsOnly
+      )
       await addMessageToQueue(randomTicketId, INITIATE_ATHENA_QUERY_QUEUE_URL)
 
       const ATHENA_EVENT_HANDLER_MESSAGE = 'Handling Athena Query event'
@@ -64,7 +76,66 @@ describe('Athena Query SQL generation and execution', () => {
       assertEventPresent(athenaQueryEvents, ATHENA_SQL_GENERATED_MESSAGE)
       assertEventPresent(athenaQueryEvents, ATHENA_INITIATED_QUERY_MESSAGE)
 
-      //Athena query id should now be in dynamodb
+      const value = await getValueFromDynamoDB(randomTicketId, 'athenaQueryId')
+      expect(value?.athenaQueryId.S).toBeDefined()
+    })
+
+    it('Successful Athena processing - requests having only PII type', async () => {
+      console.log('Test ticket id: ' + randomTicketId)
+      await populateDynamoDBWithTestItemDetails(
+        randomTicketId,
+        dynamoDBItemPIITypesOnly
+      )
+      await addMessageToQueue(randomTicketId, INITIATE_ATHENA_QUERY_QUEUE_URL)
+
+      const ATHENA_EVENT_HANDLER_MESSAGE = 'Handling Athena Query event'
+      const GENERATING_ATHENA_SQL_MESSAGE = 'Generating Athena SQL query string'
+      const ATHENA_SQL_GENERATED_MESSAGE = 'Athena SQL generated'
+      const ATHENA_INITIATED_QUERY_MESSAGE =
+        'Athena query execution initiated with QueryExecutionId'
+
+      const athenaQueryEvents =
+        await getCloudWatchLogEventsGroupByMessagePattern(
+          INITIATE_ATHENA_QUERY_LAMBDA_LOG_GROUP,
+          [ATHENA_EVENT_HANDLER_MESSAGE, 'body', randomTicketId]
+        )
+
+      expect(athenaQueryEvents).not.toEqual([])
+      expect(athenaQueryEvents.length).toBeGreaterThan(1)
+      assertEventPresent(athenaQueryEvents, GENERATING_ATHENA_SQL_MESSAGE)
+      assertEventPresent(athenaQueryEvents, ATHENA_SQL_GENERATED_MESSAGE)
+      assertEventPresent(athenaQueryEvents, ATHENA_INITIATED_QUERY_MESSAGE)
+
+      const value = await getValueFromDynamoDB(randomTicketId, 'athenaQueryId')
+      expect(value?.athenaQueryId.S).toBeDefined()
+    })
+
+    it('Successful Athena processing - requests having both data paths and PII types', async () => {
+      console.log('Test ticket id: ' + randomTicketId)
+      await populateDynamoDBWithTestItemDetails(
+        randomTicketId,
+        dynamoDBItemDataPathAndPIITypes
+      )
+      await addMessageToQueue(randomTicketId, INITIATE_ATHENA_QUERY_QUEUE_URL)
+
+      const ATHENA_EVENT_HANDLER_MESSAGE = 'Handling Athena Query event'
+      const GENERATING_ATHENA_SQL_MESSAGE = 'Generating Athena SQL query string'
+      const ATHENA_SQL_GENERATED_MESSAGE = 'Athena SQL generated'
+      const ATHENA_INITIATED_QUERY_MESSAGE =
+        'Athena query execution initiated with QueryExecutionId'
+
+      const athenaQueryEvents =
+        await getCloudWatchLogEventsGroupByMessagePattern(
+          INITIATE_ATHENA_QUERY_LAMBDA_LOG_GROUP,
+          [ATHENA_EVENT_HANDLER_MESSAGE, 'body', randomTicketId]
+        )
+
+      expect(athenaQueryEvents).not.toEqual([])
+      expect(athenaQueryEvents.length).toBeGreaterThan(1)
+      assertEventPresent(athenaQueryEvents, GENERATING_ATHENA_SQL_MESSAGE)
+      assertEventPresent(athenaQueryEvents, ATHENA_SQL_GENERATED_MESSAGE)
+      assertEventPresent(athenaQueryEvents, ATHENA_INITIATED_QUERY_MESSAGE)
+
       const value = await getValueFromDynamoDB(randomTicketId, 'athenaQueryId')
       expect(value?.athenaQueryId.S).toBeDefined()
     })
@@ -103,5 +174,7 @@ describe('Athena Query SQL generation and execution', () => {
       expect(athenaQueryEvents.length).toBeGreaterThan(1)
       assertEventPresent(athenaQueryEvents, ATHENA_HANDLER_INVOKE_ERROR)
     })
+
+    /*it('Lambda should error if details have neither data paths nor pii types', async () => {})*/
   })
 })
