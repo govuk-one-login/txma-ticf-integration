48157e1f8cfd9535cfdeb0aa1594af4173bc7dd7
gdsandrewmccafferty <110031157+gdsandrewmccafferty@users.noreply.github.com>
2022-10-25 11:41:55 +0100

    Make ticket creation script independent of integration tests (#118)

--- scripts/createAndApproveZendeskTicket.ts
+++ scripts/createAndApproveZendeskTicket.ts
@@ -1,17 +1,64 @@
 import { makeApproveZendeskTicketRequest } from '../integration-tests/utils/zendesk/approveZendeskTicket'
 import { createZendeskTicket } from '../integration-tests/utils/zendesk/createZendeskTicket'
-import { validRequestData } from '../integration-tests/constants/requestData'
+import { ZendeskRequestData } from '../integration-tests/types/zendeskRequestData'
+import {
+  ZendeskFormFieldIDs,
+  ZENDESK_END_USER_NAME,
+  ZENDESK_PII_FORM_ID
+} from '../integration-tests/constants/zendeskParameters'
+import { getEnv } from '../integration-tests/utils/helpers'
+
+const generateTicketData = (): ZendeskRequestData => ({
+  request: {
+    subject: getEnv('FIXED_SUBJECT_LINE'),
+    ticket_form_id: ZENDESK_PII_FORM_ID,
+    custom_fields: [
+      {
+        id: ZendeskFormFieldIDs.PII_FORM_IDENTIFIER_FIELD_ID,
+        value: 'event_id'
+      },
+      {
+        id: ZendeskFormFieldIDs.PII_FORM_EVENT_ID_LIST_FIELD_ID,
+        value: getEnv('OVERRIDE_EVENT_IDS')
+      },
+      {
+        id: ZendeskFormFieldIDs.PII_FORM_REQUEST_DATE_FIELD_ID,
+        value: getEnv('FIXED_DATA_REQUEST_DATE')
+      },
+      {
+        id: ZendeskFormFieldIDs.PII_FORM_REQUESTED_PII_TYPE_FIELD_ID,
+        value: ['drivers_license']
+      },
+      {
+        id: ZendeskFormFieldIDs.PII_FORM_CUSTOM_DATA_PATH_FIELD_ID,
+        value: getEnv('DATA_PATHS')
+      },
+      {
+        id: ZendeskFormFieldIDs.PII_FORM_IDENTIFIER_RECIPIENT_EMAIL,
+        value: getEnv('FIXED_RECIPIENT_EMAIL')
+      },
+      {
+        id: ZendeskFormFieldIDs.PII_FORM_IDENTIFIER_RECIPIENT_NAME,
+        value: ZENDESK_END_USER_NAME
+      }
+    ],
+    comment: {
+      body: 'PII request created by command-line tool'
+    }
+  }
+})
 
 const createAndApproveTicket = async () => {
   checkEnvironmentVariablesSet([
     'FIXED_SUBJECT_LINE',
     'FIXED_RECIPIENT_EMAIL',
     'DATA_PATHS',
-    'FIXED_DATA_REQUEST_DATE'
+    'FIXED_DATA_REQUEST_DATE',
+    'OVERRIDE_EVENT_IDS'
   ])
-
-  console.log(`creating ticket with data ${JSON.stringify(validRequestData)}`)
-  const ticketId = await createZendeskTicket(validRequestData)
+  const ticketData = generateTicketData()
+  console.log(`creating ticket with data ${JSON.stringify(ticketData)}`)
+  const ticketId = await createZendeskTicket(ticketData)
   await makeApproveZendeskTicketRequest(ticketId)
   console.log(`approved ticket with id ${ticketId}`)
 }
