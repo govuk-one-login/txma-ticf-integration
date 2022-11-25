81922e7ebfe4aad7ef9eeba94418b0218f0ab667
Andy Langridge <andy.langridge@digital.cabinet-office.gov.uk>
2022-11-25 14:40:10 +0000

    Add pipeline to upload to ECR and ensure tests run within Docker (#178)

--- integration-tests/run-tests.sh
+++ integration-tests/run-tests.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# This script will only run in AWS Codepipeline. It has access to the following environment variables:
+# CFN_<OUTPUT-NAME> - Stack output value (replace <OUTPUT-NAME> with the name of the output)
+# TEST_REPORT_ABSOLUTE_DIR - Absolute path to where the test report file should be placed
+# TEST_REPORT_DIR - Relative path from current directory to where the test report file should be placed
+# TEST_ENVIRONMENT - The environment the pipeline is running the tests in
+
 yarn test:integration
 
-cp integration-tests/reports/allure-results/junit.xml $TEST_REPORT_ABSOLUTE_DIR/junit.xml
+TESTS_EXIT_CODE=$?
+
+cp reports/allure-results/junit.xml $TEST_REPORT_ABSOLUTE_DIR/junit.xml
+
+if [ $TESTS_EXIT_CODE -ne 0 ]; then
+  exit 1
+fi
