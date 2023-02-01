0919d64454f64d8f7e749235afb6f41de3657a2e
andrewmccafferty <andrew.mccafferty@digital.cabinet-office.gov.uk>
2023-02-01 16:13:37 +0000

    Add test queue handler function

---
+++ src/lambdas/testQueueHandler/handler.ts
@@ -0,0 +1,13 @@
+import { Context, SQSEvent } from 'aws-lambda'
+import { getEnv } from '../../utils/helpers'
+import { initialiseLogger, logger } from '../../sharedServices/logger'
+
+export const handler = async (event: SQSEvent, context: Context) => {
+  initialiseLogger(context)
+  logger.info('Handling test SQS event', {
+    handledEvent: event
+  })
+  if (getEnv('SHOULD_ERROR')) {
+    throw Error('Something went wrong: ARRRRGH!')
+  }
+}
