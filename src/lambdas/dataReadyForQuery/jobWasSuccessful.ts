eda5f4d53c513643eec4cd6fe8c3d9b77edc76d6
andrewmccafferty <andrew.mccafferty@digital.cabinet-office.gov.uk>
2023-02-27 09:33:11 +0000

    Add some comments to clarify job success/failure

--- src/lambdas/dataReadyForQuery/jobWasSuccessful.ts
+++ src/lambdas/dataReadyForQuery/jobWasSuccessful.ts
@@ -5,9 +5,14 @@ export const jobWasSuccessful = async (
   jobId: string,
   status: string
 ): Promise<boolean> => {
+  // We found that the status could be "Completed" even
+  // when all the individual tasks had failed. A status of "failed" generally
+  // seems to mean that the job didn't start properly.
   if (status === 'Failed') {
     return false
   }
+  // If the status isn't "Failed" we instead make sure that the total number of tasks
+  // equals the number of succeeded tasks.
   const job = await describeBatchJob(jobId)
   logger.info('Got batch job description', { job })
   return (
