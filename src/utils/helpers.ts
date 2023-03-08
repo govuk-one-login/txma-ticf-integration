6a63f4859610b32a00d2330a2de289cf0dffe879
andrewmccafferty <andrew.mccafferty@digital.cabinet-office.gov.uk>
2023-03-08 18:04:44 +0000

    Clarify lack of logging detail of JSON errors

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -34,6 +34,9 @@ export const tryParseJSON = (jsonString: string) => {
   try {
     return JSON.parse(jsonString)
   } catch (error) {
+    // We deliberately don't log out the specific error here,
+    // because Node 19 and onwards will include part of the string the parser
+    // was attempting to parse, which can leak sensitive data
     logger.error('Error parsing JSON')
     return {}
   }
