f4b8c5596477aeff1a3efd2a4c8f7c9fc26e9576
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2023-08-10 11:28:54 +0100

    update casing

--- src/sharedServices/s3/copyS3Object.ts
+++ src/sharedServices/s3/copyS3Object.ts
@@ -7,15 +7,15 @@ import { getEnv } from '../../utils/helpers'
 
 export const copyS3Object = async (
   key: string,
-  CopySource: string,
-  Bucket: string
+  copySource: string,
+  bucket: string
 ): Promise<void> => {
   const s3Client = new S3Client({ region: getEnv('AWS_REGION') })
 
   const copyCommand: CopyObjectCommandInput = {
     Key: key,
-    CopySource: CopySource,
-    Bucket: Bucket
+    CopySource: copySource,
+    Bucket: bucket
   }
   // console.log(copyCommand)
   await s3Client.send(new CopyObjectCommand(copyCommand))
