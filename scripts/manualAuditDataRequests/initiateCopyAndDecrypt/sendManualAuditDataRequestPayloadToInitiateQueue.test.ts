c462dff3dc9b67f53f324bf5981b6ecdc68227cf
Andy Langridge <andy.langridge@digital.cabinet-office.gov.uk>
2023-09-05 12:06:42 +0100

    Fix unit tests

--- scripts/manualAuditDataRequests/initiateCopyAndDecrypt/sendManualAuditDataRequestPayloadToInitiateQueue.test.ts
+++ scripts/manualAuditDataRequests/initiateCopyAndDecrypt/sendManualAuditDataRequestPayloadToInitiateQueue.test.ts
@@ -1,24 +1,32 @@
-import { sendSqsMessage } from '../../../src/sharedServices/queue/sendSqsMessage'
-import { MOCK_INITIATE_DATA_REQUEST_QUEUE_URL } from '../../../src/utils/tests/testConstants'
+import { when } from 'jest-when'
+import { sendSqsMessage } from '../../sharedServices/aws/sqs/sendSqsMessage'
+import { getAwsAccountNumber } from '../../sharedServices/aws/sts/getAwsAccountNumber'
 import { ManualAuditDataRequestPayload } from '../../types/manualAuditDataRequestPayload'
 import { sendManualAuditDataRequestPayloadToInitiateQueue } from './sendManualAuditDataRequestPayloadToInitiateQueue'
 
-jest.mock('../../../src/sharedServices/queue/sendSqsMessage', () => ({
+jest.mock('../../sharedServices/aws/sqs/sendSqsMessage', () => ({
   sendSqsMessage: jest.fn()
+}))
+
+jest.mock('../../sharedServices/aws/sts/getAwsAccountNumber', () => ({
+  getAwsAccountNumber: jest.fn()
 }))
 
 describe('sendManualAuditDataRequestPayloadToInitiateQueue', () => {
   it('should send the payload to the initiate queue', async () => {
+    const mockAccountNumber = '123456789012'
+    const queueUrl = `https://sqs.eu-west-2.amazonaws.com/${mockAccountNumber}/txma-ticf-integration-initiate-data-request-queue`
     const testPayload = {
       zendeskId: 'MR123456789',
       dates: ['2020-01-01']
     }
+
+    when(getAwsAccountNumber).mockResolvedValue(mockAccountNumber)
+
     await sendManualAuditDataRequestPayloadToInitiateQueue(
       testPayload as ManualAuditDataRequestPayload
     )
-    expect(sendSqsMessage).toHaveBeenCalledWith(
-      testPayload,
-      MOCK_INITIATE_DATA_REQUEST_QUEUE_URL
-    )
+
+    expect(sendSqsMessage).toHaveBeenCalledWith(testPayload, queueUrl)
   })
 })
