e64b03f2461dfc2e464b5df276bc4248fc6c0df5
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2023-10-11 09:23:12 +0100

    add support for bulk date range

--- scripts/manualAuditDataRequestInitiateCopyAndDecrypt.ts
+++ scripts/manualAuditDataRequestInitiateCopyAndDecrypt.ts
@@ -1,4 +1,8 @@
 import { program } from 'commander'
+import {
+  convertDaterangeToDateArray,
+  isDateRangeArray
+} from './manualAuditDataRequests/initiateCopyAndDecrypt/daterange'
 import { generateInitiateCopyAndDecryptPayload } from './manualAuditDataRequests/initiateCopyAndDecrypt/generateInitiateCopyAndDecryptPayload'
 import { isDatesArray } from './manualAuditDataRequests/initiateCopyAndDecrypt/isDatesArray'
 import { sendManualAuditDataRequestPayloadToInitiateQueue } from './manualAuditDataRequests/initiateCopyAndDecrypt/sendManualAuditDataRequestPayloadToInitiateQueue'
@@ -7,25 +11,41 @@ import { AWS_REGION } from './utils/constants'
 process.env.AWS_REGION = AWS_REGION
 
 program
-  .requiredOption(
-    '--dates [dates...]',
-    'The dates of audit files to copy for analysis in the format "YYYY-MM-DD"'
+  .addHelpText(
+    'afterAll',
+    '\nNote that at least one of "dates" or "daterange" should be specified'
   )
   .requiredOption('--zendeskId <id>', 'The Zendesk ticket id for the request')
+  .option(
+    '--dates [dates...]',
+    'An array of dates of audit files to copy for analysis in the format "YYYY-MM-DD"'
+  )
+  .option(
+    '--daterange [daterange...]',
+    'An array of date ranges from earliest to latest of audit files to copy for analysis in the format "YYYY/MM/DD-YYYY/MM/DD"'
+  )
 
 program.parse(process.argv)
 
 const options = program.opts()
-const dates: string[] = options.dates
 const zendeskId: string = options.zendeskId
+const dates: string[] = options.dates || []
+const daterange: string[] = options.daterange
+
+if (isDateRangeArray(daterange)) {
+  dates.push(...convertDaterangeToDateArray(daterange))
+} else {
+  console.error('invalid daterange. please check the format')
+  throw new Error('Invalid Date Range')
+}
 
 if (!isDatesArray(dates) || !zendeskId) {
   console.error(
     'Invalid input parameters, please ensure all parameters are assigned the correct value'
   )
 } else {
-  const payload = generateInitiateCopyAndDecryptPayload(dates, zendeskId)
-
+  const payload = generateInitiateCopyAndDecryptPayload(dates.sort(), zendeskId)
+  console.log(payload)
   sendManualAuditDataRequestPayloadToInitiateQueue(payload)
     .then(() => console.log('Sent SQS payload to initiate queue'))
     .catch((error: unknown) => {
---
+++ scripts/manualAuditDataRequests/initiateCopyAndDecrypt/daterange.ts
@@ -0,0 +1,48 @@
+const daterangeRegex = /\d{4}\/\d{2}\/\d{2}-\d{4}\/\d{2}\/\d{2}/
+
+export const isDateRangeArray = (daterangeArray: string[]) => {
+  return (
+    Array.isArray(daterangeArray) &&
+    daterangeArray.length > 0 &&
+    daterangeArray.every(
+      (date) => typeof date === 'string' && daterangeRegex.test(date)
+    )
+  )
+}
+
+export const convertDaterangeToDateArray = (daterangeArray: string[]) => {
+  const dateRangeArray = daterangeArray.map((daterange) => {
+    const dates = daterange.split('-')
+    // const startDateObj = new Date(dates[0])
+    const endDateObj = new Date(dates[1])
+
+    // if (startDateObj.valueOf() <= endDateObj.valueOf()) {
+    //   console.error(`start date greater than end date for: ${daterange}`)
+    //   throw new Error('Invalid Date Range')
+    // }
+
+    const dateArray: Date[] = []
+    dateArray.push(new Date(dates[0]))
+
+    while (dateArray[dateArray.length - 1].valueOf() < endDateObj.valueOf()) {
+      const dateIncreasedByOne = addDay(dateArray[dateArray.length - 1])
+      dateArray.push(dateIncreasedByOne)
+    }
+    return dateArray
+  })
+
+  const flatDateArray = dateRangeArray.flat()
+  const datesAsString = flatDateArray.map((date) => {
+    return `${date.getFullYear()}-${(date.getMonth() + 1)
+      .toString()
+      .padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
+  })
+
+  return Array.from(new Set(datesAsString))
+}
+
+const addDay = (date: Date) => {
+  const newDate = new Date(date.valueOf())
+  newDate.setDate(newDate.getDate() + 1)
+  return newDate
+}
