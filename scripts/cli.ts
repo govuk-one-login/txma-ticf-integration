e51a14b5f6043d23672dc3976a5e42bbea785031
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2023-10-26 11:38:01 +0100

    refactor manual send audit data cli

---
+++ scripts/cli.ts
@@ -0,0 +1,31 @@
+import { Option, program } from '@commander-js/extra-typings'
+import { sendAuditDataAction } from './manualAuditDataRequests/sendResults/sendAuditDataAction'
+import { AWS_REGION } from './utils/constants'
+
+process.env.AWS_REGION = AWS_REGION
+program
+  .name('ticf-integration-cli')
+  .description('cli tool for txma ticfontegration')
+
+program
+  .command('send-audit-data')
+  .description('Uses SAL to send output of athena query to users via email')
+  .addOption(
+    new Option(
+      '-e, --environment <env>',
+      'The environment to run the script in'
+    )
+      .choices(['dev', 'build', 'staging', 'integration', 'production'])
+      .makeOptionMandatory()
+  )
+  .requiredOption(
+    '--athenaQueryId <id>',
+    'The athenaQuery Id of the query that was ran against the audit data'
+  )
+  .requiredOption('--zendeskId <id>', 'The Zendesk ticket id for the request')
+  .requiredOption('--recipientName <name>', 'The recipient name')
+  .requiredOption('--recipientEmail <email>', 'The recipient email')
+  .action((options) => {
+    sendAuditDataAction({ ...options }).then(() => {})
+  })
+program.parse(process.argv)
