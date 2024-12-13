43bcbb91b049d4b86e5f6ff382ea24025fbc3cdc
hdavey-gds <129174608+hdavey-gds@users.noreply.github.com>
2024-12-13 14:06:05 +0000

    Queue URL Fix (#461)
    Use AWS SDK to get queue URL from name and pass to sendSqsMessage
    
    Co-authored-by: zaydgds <113358498+zaydgds@users.noreply.github.com>

--- scripts/sendQueryResults/sendSQSMessageToCompletedQueue.ts
+++ scripts/sendQueryResults/sendSQSMessageToCompletedQueue.ts
@@ -1,10 +1,24 @@
 import { sendSqsMessage } from '../../src/sharedServices/queue/sendSqsMessage'
 import { SendManualQueryPayload } from '../types/sendManualQueryPayload'
+import { getEnv } from '../../src/utils/helpers'
+import { GetQueueUrlCommand, SQSClient } from '@aws-sdk/client-sqs'
 
 export const sendSQSMessageToCompletedQueue = async (
   environment: string,
   manualQueryCompleteSqsPayload: SendManualQueryPayload
 ): Promise<void> => {
-  const queueUrl = `txma-data-analysis-${environment}-query-completed-queue`
+  const queueUrl = await getCompletedQueueUrl(environment)
   await sendSqsMessage(manualQueryCompleteSqsPayload, queueUrl)
+}
+
+const getCompletedQueueUrl = async (environment: string): Promise<string> => {
+  const client = new SQSClient({ region: getEnv('AWS_REGION') })
+  const queueName = `txma-data-analysis-${environment}-query-completed-queue`
+  const queueUrlResponse = await client.send(
+    new GetQueueUrlCommand({ QueueName: queueName })
+  )
+  if (queueUrlResponse.QueueUrl === undefined) {
+    throw new Error('QueueUrl was undefined')
+  }
+  return queueUrlResponse.QueueUrl
 }
